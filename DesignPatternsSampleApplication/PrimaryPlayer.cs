8e75261401e9359dd79e57708966949fb7ff1e71
Joshua Richardson <joshua.richardson@myunidays.com>
2021-08-09 17:03:49 +0100

    Added singleton + comments

---
+++ DesignPatternsSampleApplication/PrimaryPlayer.cs
@@ -0,0 +1,54 @@
+﻿namespace DesignPatternsSampleApplication
+{
+    /// This is a singleton
+    /// Could be seen to violate the Single Responsibility principle - has to create itself - extra responsibilities
+    /// Better approach could be to use dependency injection
+    public sealed class PrimaryPlayer
+    {
+
+        /// <summary>
+        /// A reference to the player - must be private so that it doesn't get modified
+        /// Use a getter to access it publicly
+        /// </summary>
+        private static readonly PrimaryPlayer _instance;
+
+        /// <summary>
+        /// Used before first use of a class
+        /// Initialise the PrimaryPlayer only when we need it and only once
+        /// Benefits:
+        /// Lazy loading - initialise only when needed
+        /// Thread safety - only runs once in whole domain
+        /// </summary>
+        static PrimaryPlayer()
+        {
+            _instance = new PrimaryPlayer()
+            {
+                Name = "Joshua",
+                Level = 1
+            };
+        }
+        
+        
+        public string Name { get; set; }
+        public int Level { get; set; }
+
+        /// Private constructor - prevent it from being instantiated
+        /// But now no access to the player at all
+        private PrimaryPlayer()
+        {
+            
+        }
+
+        /// <summary>
+        ///  Return readonly backing field - need to have an instance to return
+        /// Allow the instance to be accessed
+        /// </summary>
+        public static PrimaryPlayer Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
