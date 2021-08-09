b53ee16b4193f13d393bacff9f1468e5d8967bc3
Joshua Richardson <joshua.richardson@myunidays.com>
2021-08-09 16:52:34 +0100

    Initial commit

---
+++ DesignPatternsSampleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatternsSampleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
